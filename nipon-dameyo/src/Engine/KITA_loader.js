308a88ba5f3564569f26858eccd960d9da2021f4
강찬영 <backgwa@icloud.com>
2024-07-03 01:41:40 +0900

    update

--- nipon-dameyo/src/Engine/KITA_loader.js
+++ nipon-dameyo/src/Engine/KITA_loader.js
@@ -1,7 +1,17 @@
+
 try {
     window.onload = () => {
         init();
+        // debug();
     }
 } catch (e) {
     console.error(e);
+}
+
+function debug() {
+    if (document.body.classList.contains('debugmode')) {
+        document.body.classList.remove('debugmode');
+    } else {
+        document.body.classList.add('debugmode');
+    }
 }
